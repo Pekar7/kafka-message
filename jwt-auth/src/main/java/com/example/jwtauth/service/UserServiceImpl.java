69846cb31ed2a700efc3103fca06be8e4ebbd531
Pekar7 <k1901321@gmail.com>
2023-09-03 16:58:35 +0300

    v3 Add log4j

--- jwt-auth/src/main/java/com/example/jwtauth/service/UserServiceImpl.java
+++ jwt-auth/src/main/java/com/example/jwtauth/service/UserServiceImpl.java
@@ -4,11 +4,13 @@ import com.example.jwtauth.model.CasinoUser;
 import com.example.jwtauth.model.UserRole;
 import com.example.jwtauth.repository.CasinoUserRepository;
 import com.example.jwtauth.repository.UserRoleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
@@ -23,11 +25,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public CasinoUser saveUser(CasinoUser casinoUser) {
+        log.info("Saving new user " + casinoUser.getUsername());
         return casinoUserRepository.save(casinoUser);
     }
 
     @Override
     public UserRole saveRole(UserRole userRole) {
+        log.info("Saving new role " + userRole.getName());
         return casinoUserRoleRepository.save(userRole);
     }
 
@@ -35,16 +39,19 @@ public class UserServiceImpl implements UserService {
     public void addRoleToUser(String username, String roleName) {
         CasinoUser casinoUser = casinoUserRepository.findCasinoUserBy(username);
         UserRole userRole = casinoUserRoleRepository.findByName(roleName);
+        log.info("Adding role {} to user {}", userRole.getName(), casinoUser.getUsername());
         casinoUser.getRoles().add(userRole);
     }
 
     @Override
     public CasinoUser getUser(String username) {
+        log.info("Find user from DB with username {}", username);
         return casinoUserRepository.findCasinoUserBy(username);
     }
 
     @Override
     public List<CasinoUser> getUsers() {
+        log.info("Get all user from DB");
         return casinoUserRepository.findAll();
     }
 }
