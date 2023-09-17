efa4fdb6b7534377543ba1c8514a894a373fc580
Pekar7 <k1901321@gmail.com>
2023-09-17 15:00:06 +0300

    init commit

---
+++ kafka-micro/src/main/java/com/example/kafkamicro/KafkaMicroApplication.java
@@ -0,0 +1,12 @@
+package com.example.kafkamicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMicroApplication.class, args);
+    }
+}
---
+++ kafka-micro/src/test/java/com/example/kafkamicro/KafkaMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkamicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMicroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
