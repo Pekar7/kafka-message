1192a654f1346af4047d0fe4e64214e6a216c251
Karen Petrosian <90376574+Pekar7@users.noreply.github.com>
2023-10-30 09:31:51 +0300

    Delete jwt-auth directory

--- jwt-auth/src/main/java/com/example/jwtauth/repository/UserRoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.jwtauth.repository;
-
-import com.example.jwtauth.model.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
-    UserRole findByName(String name);
-}
